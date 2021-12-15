15a3e8bb70b505a5da80034850a59ddb50c06a7a
nganng051020 <ngan.nt184165@sis.hust.edu.vn>
2021-12-15 19:24:21 +0000

    edit coffee form

--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -2,6 +2,7 @@ class Coffee < ApplicationRecord
     has_one_attached :image 
     has_many :coffee_review, -> { order "created_at DESC"}
     # validates :title, :description, :price, :restaurant => true
+    validates :description, length: { minimum: 10 }
     validates :title, :restaurant, length: { minimum: 2 }
     validates :price, numericality: { only_integer: true }
 end
